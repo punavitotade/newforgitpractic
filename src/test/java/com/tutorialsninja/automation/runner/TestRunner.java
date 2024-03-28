02a8ba4f887e7dc94741426b22f400c412edf77f
Girish Chandankhede <girish.chandankhede@ptechno.com>
2024-03-28 13:41:40 +0530

    upadetd pom.xml file and renamed runner.java to TestRunner

---
+++ src/test/java/com/tutorialsninja/automation/runner/TestRunner.java
@@ -0,0 +1,21 @@
+package com.tutorialsninja.automation.runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features={" classpath:FeatureFiles/Login.feature",
+		                    "classpath:FeatureFiles/Order.feature",
+		                     "classpath:FeatureFiles/Register.feature",
+		                     "classpath:FeatureFiles/Search.feature"
+
+                               },
+glue={"classpath:com.tutorialsninja.automation.stepdef"},
+plugin={"html:target/cucumber_html_report"})
+
+
+public class TestRunner {
+
+}
